093c1125221247cbe62e6446803d053f787f140b
Eetu Alanen <ezka.alanen@gmail.com>
2023-11-21 20:49:23 +0200

    Added a method to the ReadingRecommendationController for rendering the list of recommendations. Also modified the MessageController class so that in listMessages method GetMapping is not the root path. Minor changes to the recommendationList.html template.

--- src/main/java/fi/haagahelia/coolreads/controller/ReadingRecommendationController.java
+++ src/main/java/fi/haagahelia/coolreads/controller/ReadingRecommendationController.java
@@ -1,15 +1,32 @@
 package fi.haagahelia.coolreads.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import fi.haagahelia.coolreads.model.ReadingRecommendation;
+
+
+import fi.haagahelia.coolreads.repository.ReadingRecommendationRepository;
 
 @Controller
 public class ReadingRecommendationController {
+    @Autowired
+    private ReadingRecommendationRepository recommendationRepository;
 
     @GetMapping("/add-recommendation")
     public String showAddRecommendationForm(Model model) {
         return "addRecommendation";
+    }
+
+    @GetMapping("/")
+    public String showRecommendationList(Model model){
+        List<ReadingRecommendation> recomendations = recommendationRepository.findAll();
+        model.addAttribute("recomendations", recomendations);
+
+        return "recommendationList";
     }
 
 }
