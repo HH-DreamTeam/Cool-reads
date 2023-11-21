4c30dca3bfe16760fe59fedd3c673b231bb3e24c
Fred Mboya <fbiko@yahoo.com>
2023-11-22 01:32:53 +0200

    updated ReadingRecommendationController and recommendationList imports

--- src/main/java/fi/haagahelia/coolreads/controller/ReadingRecommendationController.java
+++ src/main/java/fi/haagahelia/coolreads/controller/ReadingRecommendationController.java
@@ -3,9 +3,6 @@ package fi.haagahelia.coolreads.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
