d044109138a96ca7d5020a563cc49752f40b166b
Fred Mboya <fbiko@yahoo.com>
2023-11-21 23:58:15 +0200

    Changes made

--- src/main/java/fi/haagahelia/coolreads/model/ReadingRecommendation.java
+++ src/main/java/fi/haagahelia/coolreads/model/ReadingRecommendation.java
@@ -1,12 +1,14 @@
 package fi.haagahelia.coolreads.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
+@Entity
 public class ReadingRecommendation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
