59013dcd5942388a86be93b2059b2471d85c1e14
Eetu Alanen <ezka.alanen@gmail.com>
2023-11-22 16:22:31 +0200

    Added Get and Post methods for editing recommendations to controller. Also minor changes to editRecommendation.html file.

--- src/main/java/fi/haagahelia/coolreads/controller/ReadingRecommendationController.java
+++ src/main/java/fi/haagahelia/coolreads/controller/ReadingRecommendationController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import fi.haagahelia.coolreads.model.ReadingRecommendation;
@@ -40,6 +41,34 @@ public class ReadingRecommendationController {
         List<ReadingRecommendation> recommendations = recommendationRepository.findAll();
         model.addAttribute("recommendations", recommendations);
         return "recommendationList";
+    }
+
+    @GetMapping("/recommendations/edit/{id}")
+    public String showEditRecommendationForm(@PathVariable Long id, Model model) {
+        ReadingRecommendation recommendation = recommendationRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid recommendation id: " + id));
+
+        model.addAttribute("recommendation", recommendation);
+        return "editRecommendation";
+    }
+
+    @PostMapping("/recommendations/edit/{id}")
+    public String editRecommendation(@PathVariable Long id,
+            @Valid @ModelAttribute ReadingRecommendation editedRecommendation,
+            BindingResult result) {
+        if (result.hasErrors()) {
+            return "editRecommendation";
+        }
+        ReadingRecommendation existingRecommendation = recommendationRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid recommendation id: " + id));
+
+        existingRecommendation.setTitle(editedRecommendation.getTitle());
+        existingRecommendation.setLink(editedRecommendation.getLink());
+        existingRecommendation.setDescription(editedRecommendation.getDescription());
+
+        recommendationRepository.save(existingRecommendation);
+
+        return "redirect:/";
     }
 
 }
