0101d6fb26b3e1ea5421ee8c39ee9a44b4cb70aa
Fred Mboya <fbiko@yahoo.com>
2023-11-30 01:33:17 +0200

    Adding categories navigation link to the layout template

--- src/main/java/fi/haagahelia/coolreads/model/Category.java
+++ src/main/java/fi/haagahelia/coolreads/model/Category.java
@@ -38,4 +38,10 @@ public class Category {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + "]";
+    }
+    
 }
