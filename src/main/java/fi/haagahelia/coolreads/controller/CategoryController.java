72be7e6eceecf088f981dfd23db86af35d225686
Fred Mboya <fbiko@yahoo.com>
2023-11-30 01:22:29 +0200

    Adding a CategoryController controller class and a method for rendering the category submission form and saving categories

---
+++ src/main/java/fi/haagahelia/coolreads/controller/CategoryController.java
@@ -0,0 +1,35 @@
+package fi.haagahelia.coolreads.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import fi.haagahelia.coolreads.model.Category;
+import fi.haagahelia.coolreads.repository.CategoryRepository;
+import jakarta.validation.Valid;
+
+@Controller
+public class CategoryController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping("/add-category")
+    public String showAddCategoryForm(Model model) {
+        model.addAttribute("category", new Category());
+        return "addCategory";
+    }
+
+    @PostMapping("/add-category")
+    public String addCategory(@Valid @ModelAttribute("category") Category category, BindingResult result) {
+        if (result.hasErrors()) {
+            return "addCategory";
+        }
+        categoryRepository.save(category);
+        return "redirect:/";
+    }
+}
