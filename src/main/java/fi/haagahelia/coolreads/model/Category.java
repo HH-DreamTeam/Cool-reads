b2c55681dacaf3e1c9a2a0569843cf865f53856e
Fred Mboya <fbiko@yahoo.com>
2023-11-30 19:47:07 +0200

    Added the @JsonIgnore annotation to the @OneToMany in the Category entity class

--- src/main/java/fi/haagahelia/coolreads/model/Category.java
+++ src/main/java/fi/haagahelia/coolreads/model/Category.java
@@ -2,6 +2,8 @@ package fi.haagahelia.coolreads.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +21,7 @@ public class Category {
     @NotBlank(message = "Name cannot be blank")
     private String name;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "category")
     private List<ReadingRecommendation> recommendations;
 
