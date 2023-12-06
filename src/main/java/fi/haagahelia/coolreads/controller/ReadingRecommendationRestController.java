91677be11b338d530797ed2d8a3cc8c7022edfef
Eetu Alanen <ezka.alanen@gmail.com>
2023-12-06 15:39:55 +0200

    Added tags and operations for ReadingRecommendationRestController

--- src/main/java/fi/haagahelia/coolreads/controller/ReadingRecommendationRestController.java
+++ src/main/java/fi/haagahelia/coolreads/controller/ReadingRecommendationRestController.java
@@ -8,13 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import fi.haagahelia.coolreads.model.ReadingRecommendation;
 import fi.haagahelia.coolreads.repository.ReadingRecommendationRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-
+@Tag(name = "Recommendation", description = "Information about reading recommendations")
 public class ReadingRecommendationRestController {
 	@Autowired
 	private ReadingRecommendationRepository recommendationRepository;
 
+    @Operation(
+        summary = "List of reading recommendations",
+        description = "Fetches all reading recommendations"
+    )
     @GetMapping("/api/recommendations")
     public List<ReadingRecommendation> getRecommendations() {
         return recommendationRepository.findAllByOrderByCreatedAtDesc();
