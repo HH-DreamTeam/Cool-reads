621a84b3274d2622c13b87044bfaf5be3f2bf0e5
Fred Mboya <fbiko@yahoo.com>
2023-12-07 10:37:47 +0200

    add a fetchRecommendationsByCategory function for category recommendations

--- frontend/recommendationList/fetchRecommendations.js
+++ frontend/recommendationList/fetchRecommendations.js
@@ -1,3 +1,7 @@
 export default function fetchRecommendations() {
   return fetch("/api/recommendations").then((response) => response.json());
 }
+
+export function fetchRecommendationsByCategory(categoryId) {
+  return fetch(`/api/categories/${categoryId}/recommendations`).then((response) => response.json());
+}
