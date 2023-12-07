ad648f900a097e4296ceffd806f4bb7316d49694
Fred Mboya <fbiko@yahoo.com>
2023-12-07 09:46:00 +0200

    updated the component to display a recommendation as a table row with Edit and Delete buttons

--- frontend/recommendationList/RecommendationListItem.jsx
+++ frontend/recommendationList/RecommendationListItem.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 
 export default function RecommendationListItem(props) {
-  return <li>{props.readingrecommendation.title}</li>;
+  const { recommendation } = props;
+  const { id, title, link, description, category, createdAt } = recommendation;
+
+  return (
+    <tr>
+      <td>{title}</td>
+      <td>
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          {link}
+        </a>
+      </td>
+      <td>{description}</td>
+      <td>{category ? category.name : "No Category"}</td>
+      <td>{new Date(createdAt).toLocaleDateString()}</td>
+      <td>
+        <a className="btn btn-primary" href={`/recommendations/edit/${id}`}>
+          Edit
+        </a>
+      </td>
+      <td>
+        <form method="POST" action={`/recommendations/${id}/delete`}>
+          <button type="submit" className="btn btn-danger">
+            Delete
+          </button>
+        </form>
+      </td>
+    </tr>
+  );
 }
